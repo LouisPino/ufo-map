a3613ec06f40515924a7f33e6a24fe191a1adbcd
LouisPino <ljpino95@gmail.com>
2023-11-22 15:56:49 -0500

    ignore incomplete data, check for existence of fields before writing object

--- scraper.py
+++ scraper.py
@@ -13,22 +13,23 @@ except FileNotFoundError:
     data_dict = {"data": []}
 
 
-for i in range(3):
-    html = requests.get(url + str(i + 179116))
+for i in range(20):
+    html = requests.get(url + str(i + 179115))
     soup = BeautifulSoup(html.content, 'html.parser')
     new_entry = str(soup.find("div", class_="content-area"))
     new_dict = {
         "occurred": new_entry.split("<b>Occurred:</b> ")[1].split(" Local<br/>")[0],
         "location": new_entry.split("<b>Location:</b> ")[1].split("<br/>")[0],
-        # "location_details": new_entry.split("details:</b> ")[1].split("<br/>")[0], Need to check whether or not details exist first
+        "location_details": new_entry.split("details:</b> ")[1].split("<br/>")[0] if "Location details" in new_entry else "",
         "shape": new_entry.split("<b>Shape:</b> ")[1].split("<br/>")[0],
         "duration": new_entry.split("<b>Duration:</b> ")[1].split("<br/>")[0],
-        "observers": new_entry.split("<b>No of observers:</b> ")[1].split("<br/>")[0],
+        "observers": new_entry.split("<b>No of observers:</b> ")[1].split("<br/>")[0] if "No of observers" in new_entry else "",
         "reported": new_entry.split("<b>Reported:</b> ")[1].split("<br/>")[0],
         "posted": new_entry.split("<b>Posted:</b> ")[1].split("<br/>")[0],
-        "characteristics": new_entry.split("<b>Characteristics:</b> ")[1].split("<p style=\"color: white;")[0].replace("<br/>", "").replace("<br>", "")     
+        "characteristics": new_entry.split("<b>Characteristics:</b> ")[1].split("<p style=\"color: white;")[0].replace("<br/>", "").replace("<br>", "") if "Characteristics" in new_entry else ""    
         }
-    data_dict["data"].append(new_dict)
+    if new_dict["occurred"] != "":
+        data_dict["data"].append(new_dict)
 
 
 # Write the updated dictionary back to the file in proper JSON format
