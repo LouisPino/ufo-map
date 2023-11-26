ea4454f638caf78fbae8b2cc182673af2c62a76d
LouisPino <ljpino95@gmail.com>
2023-11-26 18:45:15 -0500

    worked on preliminary display functions in logger.py

--- logger.py
+++ logger.py
@@ -10,10 +10,44 @@ with open(file_path, 'r') as file:
 # You can print it or perform other operations.
 data_objs = data["data"]
 
+
+# Print location for all sightings in NY
+# for obj in data_objs:
+#     if "NY, " in obj["location"]:
+#         print(obj["location"]) 
+
+
+
+# Print amt of sightings
+# print(len(data_objs))
+
+
+# Print hash map with amt of sightings per country
+perCtry = {}
 for obj in data_objs:
-    if "NY, " in obj["location"]:
-        print(obj["location"]) 
+    ctry = obj["location"].split(",")[2]
+    if ctry in perCtry.keys():
+        perCtry[ctry]+=1
+    else:
+        perCtry[ctry]=1
+perCtry = dict(sorted(perCtry.items(), key=lambda item: item[1]))
+
+for key, value in perCtry.items():
+    # uncomment line below to see only one country / specify a rule
+    # if key == " USA":     
+         print(f"{key}: {value}")
 
 
 
+# Print all IDs of sightings from selected contry
+perCtry = {}
+for obj in data_objs:
+    ctry = obj["location"].split(",")[2]
+    if ctry in perCtry.keys():
+        perCtry[ctry].append(obj)
+    else:
+        perCtry[ctry]=[obj]
+# perCtry = dict(sorted(perCtry.items(), key=lambda item: item[1]))
 
+for sighting in perCtry[" Italy"]:
+         print(f'{sighting["id"]}')
--- scraper.py
+++ scraper.py
@@ -3,12 +3,14 @@ import json
 import aiohttp
 from aiohttp import ClientError
 import asyncio
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) FORWINDOWS ONLY
 import requests
+
+LAST_ID = 179135
 
 base_url = "https://nuforc.org/sighting/?id="
 # file_path = "ufo-data.json"
-file_path = "test.json"
+file_path = "thirtyK.json"
 # file_path = "ufo-data-long-test.json"
 
 
@@ -45,8 +47,8 @@ async def fetch_data(session, id, retry_count=3, delay=1):
                     raise e  # Reraise the exception after the last retry
 
 async def main():
-    count = 0
-    while count < 179135 :
+    count = 41701
+    while count < LAST_ID :
         async with aiohttp.ClientSession() as session:
             # Fetch data for each ID concurrently
             results = await asyncio.gather(
@@ -71,29 +73,31 @@ async def main():
                     #     vid = str(new_vids[i]).split("src=")[1].split('"')[1]
                     #     print(vid)
                     #     vid_links.append(vid)
-                    new_dict = {
-                        "id": id,
-                        "occurred": new_entry.split("<b>Occurred:</b> ")[1].split(" Local<br/>")[0] if "Occurred" in new_entry else "",
-                        "location": new_entry.split("<b>Location:</b> ")[1].split("<br/>")[0] if "Location" in new_entry else "",
-                        "location_details": new_entry.split("details:</b> ")[1].split("<br/>")[0] if "Location details" in new_entry else "",
-                        "shape": new_entry.split("<b>Shape:</b> ")[1].split("<br/>")[0] if "Shape" in new_entry else "",
-                        "duration": new_entry.split("<b>Duration:</b> ")[1].split("<br/>")[0] if "Duration" in new_entry else "",
-                        "observers": new_entry.split("<b>No of observers:</b> ")[1].split("<br/>")[0] if "No of observers" in new_entry else "",
-                        "reported": new_entry.split("<b>Reported:</b> ")[1].split("<br/>")[0] if "Reported" in new_entry else "",
-                        "posted": new_entry.split("<b>Posted:</b> ")[1].split("<br/>")[0] if "Posted" in new_entry else "",
-                        "characteristics": new_entry.split("<b>Characteristics:</b> ")[1].split("<p style=\"color: white;")[0].replace("<br/>", ". ").replace("<br>", ". ") if "Characteristics" in new_entry else "" ,   
-                        "images": img_links,
-                        # "videos": vid_links,
-                        }
-                    
-                    if new_dict["occurred"] != "" and new_dict["location"] != "":
+                    try:
+                        new_dict = {
+                            "id": id,
+                            "occurred": new_entry.split("<b>Occurred:</b> ")[1].split(" Local<br/>")[0] if "Occurred" in new_entry else "",
+                            "location": new_entry.split("<b>Location:</b> ")[1].split("<br/>")[0] if "Location" in new_entry else "",
+                            "location_details": new_entry.split("details:</b> ")[1].split("<br/>")[0] if "Location details" in new_entry else "",
+                            "shape": new_entry.split("<b>Shape:</b> ")[1].split("<br/>")[0] if "Shape" in new_entry else "",
+                            "duration": new_entry.split("<b>Duration:</b> ")[1].split("<br/>")[0] if "Duration" in new_entry else "",
+                            "observers": new_entry.split("<b>No of observers:</b> ")[1].split("<br/>")[0] if "No of observers" in new_entry else "",
+                            "reported": new_entry.split("<b>Reported:</b> ")[1].split("<br/>")[0] if "Reported" in new_entry else "",
+                            "posted": new_entry.split("<b>Posted:</b> ")[1].split("<br/>")[0] if "Posted" in new_entry else "",
+                            "characteristics": new_entry.split("<b>Characteristics:</b> ")[1].split("<p style=\"color: white;")[0].replace("<br/>", ". ").replace("<br>", ". ") if "Characteristics" in new_entry else "" ,   
+                            "images": img_links,
+                            # "videos": vid_links,
+                            }
+                    except:
+                        continue
+                    if new_dict["occurred"] != "" and new_dict["location"] != ", , ":
                         data_dict["data"].append(new_dict)
                     # Write the updated dictionary back to the file in proper JSON format
                     
         with open(file_path, "w") as outfile:
             json.dump(data_dict, outfile, indent=4)   
-        print(f"Read up to {count}")
         count+=1000
+        print(f"Read up to {count}")
                     
 
 
@@ -102,19 +106,9 @@ async def main():
 
 
 asyncio.run(main()) 
-
-
-# # Write the updated dictionary back to the file in proper JSON format
-# with open(file_path, "w") as outfile:
-#     json.dump(data_dict, outfile, indent=4)   
-    
    
     
 # to do:
 # get videos
 # Find efficient way to check if ID is already in list (maybe keep it sorted then binary search?)
-# error handling so you can just go through the whole site.
-# server disconnect handling
-# Just write to a database instead?
-# Some don't have a characteristics title but do have descriptions, get this somehow
-# the word characteristics may exist in description, account for this
+# # Some don't have a characteristics title but do have descriptions, get this somehow
