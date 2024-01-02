a092840ac5b70a63ac06d1d6bd28d4f5f1f96114
LouisPino <ljpino95@gmail.com>
2024-01-02 12:50:06 -0500

    convert logger functions to CL args

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 import json
+import sys
 
-file_path = 'Scrape1.json'
+file_path = 'Scrape2.json'
 
 with open(file_path, 'r') as file:
     data = json.load(file)
@@ -8,86 +9,99 @@ with open(file_path, 'r') as file:
 data_objs = data["data"]
 
 
-# # Print location for all sightings in NY
-# for obj in data_objs:
-#     if "NY, " in obj["location"]:
-#         print(obj["location"]) 
+# Print location for all sightings in given location
+# Usage: python3 logger.py location NY
+if sys.argv[1] == "location":
+    for obj in data_objs:
+        if f"{sys.argv[2]}, " in obj["location"]:
+            print(obj["location"]) 
 
 
-# # Print amt of sightings
-# print(len(data_objs))
+# Print amt of sightings
+# Usage: python3 logger.py length
+if sys.argv[1] == "length":
+    print(len(data_objs))
 
 
-# # Print hash map with amt of sightings per country
-# perCtry = {}
-# for obj in data_objs:
-#     ctry = obj["location"].split(",")[2]
-#     if ctry in perCtry.keys():
-#         perCtry[ctry]+=1
-#     else:
-#         perCtry[ctry]=1
-# perCtry = dict(sorted(perCtry.items(), key=lambda item: item[1]))
-
-# for key, value in perCtry.items():
-#     # uncomment line below to see only one country / specify a rule
-#     # if key == " USA":     
-#          print(f"{key}: {value}")
+# Print map with amt of sightings per country
+# Usage: python3 logger.py country
+if sys.argv[1] == "country":
+    perCtry = {}
+    for obj in data_objs:
+        ctry = obj["location"].split(",")[2].capitalize()
+        if ctry in perCtry.keys():
+            perCtry[ctry]+=1
+        else:
+            perCtry[ctry]=1
+    perCtry = dict(sorted(perCtry.items(), key=lambda item: item[1]))
 
 
-
-# # Print all IDs of sightings from selected contry
-# perCtry = {}
-# for obj in data_objs:
-#     ctry = obj["location"].split(",")[2]
-#     if ctry in perCtry.keys():
-#         perCtry[ctry].append(obj)
-#     else:
-#         perCtry[ctry]=[obj]
- 
-# for sighting in perCtry[" Spain"]:
-#          print(f'{sighting["id"]}')
+# Print all IDs of sightings from selected country
+# Usage: python3 logger.py in Spain
+if sys.argv[1] == "in":
+    perCtry = {}
+    for obj in data_objs:
+        ctry = obj["location"].split(",")[2]
+        if ctry in perCtry.keys():
+            perCtry[ctry].append(obj)
+        else:
+            perCtry[ctry]=[obj]
+    
+    for sighting in perCtry[f" {sys.argv[2]}"]:
+            print(f'{sighting["id"]}')
 
 # Get amount with images
-# img_count = 0
-# for obj in data_objs:
-#     if len(obj["images"]) > 0:
-#         img_count += 1      
-# print(img_count)
+# Usage: python3 logger.py image
+if sys.argv[1] == "image":
+    img_count = 0
+    for obj in data_objs:
+        if len(obj["images"]) > 0:
+            img_count += 1      
+    print(img_count)
+
 
 # Get IDs of sightings with videos
-# vid_ids = []
-# for obj in data_objs:
-#     if  "#museai-player" in obj["characteristics"]:
-#         vid_ids.append(obj["id"])
-# print(vid_ids)
-
-# write to file if needed
-# # with open("vid_list.txt", "w") as outfile:
-# #     json.dump(vid_ids, outfile, indent=4)   
+# Usage: python3 logger.py video
+if sys.argv[1] == "video":
+    vid_ids = []
+    for obj in data_objs:
+        if  "#museai-player" in obj["characteristics"]:
+            vid_ids.append(obj["id"])
+    print(vid_ids) 
 
 
-# # Print shapes hash
-# shapes = {}
-# for obj in data_objs:
-#     shape = obj["shape"].lower()
-#     if shape in shapes.keys():
-#         shapes[shape]+=1
-#     else:
-#         shapes[shape]=1
-# shapes = dict(sorted(shapes.items(), key=lambda item: item[1]))
 
-# for key, value in shapes.items():    
-#      print(f"{key}: {value}")
 
-# # Print observers hash
-# obs_map = {}
-# for obj in data_objs:
-#     observers = obj["observers"].lower()
-#     if observers in obs_map.keys():
-#         obs_map[observers]+=1
-#     else:
-#         obs_map[observers]=1
-# obs_map = dict(sorted(obs_map.items(), key=lambda item: item[1]))
+# Print shapes map
+# Usage: python3 logger.py shape
 
-# for key, value in obs_map.items():    
-#      print(f"{key}: {value}")
+if sys.argv[1] == "shape":
+    shapes = {}
+    for obj in data_objs:
+        shape = obj["shape"].lower()
+        if shape in shapes.keys():
+            shapes[shape]+=1
+        else:
+            shapes[shape]=1
+    shapes = dict(sorted(shapes.items(), key=lambda item: item[1]))
+
+    for key, value in shapes.items():    
+        print(f"{key}: {value}")
+
+
+
+
+# # Print observers map
+# Usage: python3 logger.py observer
+if sys.argv[1] == "observer":
+    obs_map = {}
+    for obj in data_objs:
+        observers = obj["observers"].lower()
+        if observers in obs_map.keys():
+            obs_map[observers]+=1
+        else:
+            obs_map[observers]=1
+    obs_map = dict(sorted(obs_map.items(), key=lambda item: item[1]))
+
+    for key, value in obs_map.items():    
+        print(f"{key}: {value}")
--- vid_scraper.py
+++ vid_scraper.py
@@ -1,8 +1,6 @@
 import json
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 import re
 
 
@@ -10,6 +8,12 @@ video_ids = []
 
 with open("vid_list.txt") as vid_list:
     video_ids = json.load(vid_list)
+
+
+
+
+
+# Modify json to track which videos have (1) videos or not (0)
 
 with open("Scrape2.json") as scrape2:
     scrape_data = json.load(scrape2)
@@ -24,14 +28,7 @@ with open("Scrape2.json", "w") as outfile:
             json.dump(scrape_data, outfile, indent=4)   
 
 
-# Selenium 
-# driver = webdriver.Chrome()
-# driver.get(f"https://nuforc.org/sighting/?id={video_ids[0]}")
-# assert "NUFORC" in driver.title
-# pattern = re.compile(r"museai-player")
-# elements = driver.find_elements(By.TAG_NAME, "div")
-# video_player = [elem for elem in elements if pattern.search(elem.get_attribute('id'))][0]
-# print(video_player)
-# driver.close()
 
-# Videos are blocked by Muse.ai video host. :( Will just have to link to pages that have videos :(
+
+
+# videos are blocked by Muse.ai video host. :( Will just have to link to pages that have videos :(
